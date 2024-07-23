092b3828d8b0322393d01ce26f8f03e7492231b9
Joel Guzman <jolman009@yahoo.com>
2024-07-22 22:43:43 -0500

    com

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -12,7 +12,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
     <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Customer Name */}
+         Customer Name 
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
@@ -37,7 +37,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           </div>
         </div>
 
-        {/* Invoice Amount */}
+         Invoice Amount 
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
             Choose an amount
@@ -57,7 +57,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           </div>
         </div>
 
-        {/* Invoice Status */}
+         Invoice Status 
         <fieldset>
           <legend className="mb-2 block text-sm font-medium">
             Set the invoice status
